ca3e2bde316a6e05aeead4b529f6e875f1b1df8d
hakutaku <qq20099@outlook.com>
2023-11-26 17:53:59 +0800

    add Penguin-Subtitle-Player

--- default.nix
+++ default.nix
@@ -28,6 +28,7 @@
   cider = pkgs.callPackage ./pkgs/cider { };
   rescrobbled = pkgs.callPackage ./pkgs/rescrobbled { };
   swgp-go = pkgs.callPackage ./pkgs/swgp-go { };
+  Penguin-Subtitle-Player  = pkgs.libsForQt5.callPackage ./pkgs/Penguin-Subtitle-Player { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
