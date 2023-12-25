1c3c8a8e127ea8e594aa61d6ac69cfade5d49fdc
hakutaku <qq20099@outlook.com>
2023-12-25 20:47:35 +0800

    fix

--- pkgs/hydrogen-music/default.nix
+++ pkgs/hydrogen-music/default.nix
@@ -15,7 +15,7 @@ appimageTools.wrapType2 rec {
     in
     ''
 
-    install -m 444 -D ${contents}/${pname}.desktop -t $out/share/applications
+    #install -m 444 -D ${contents}/${pname}.desktop -t $out/share/applications
     substituteInPlace $out/share/applications/${pname}.desktop \
       --replace 'Exec=AppRun' 'Exec=${pname}'
     cp -r ${contents}/usr/share/icons $out/share
