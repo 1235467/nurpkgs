795a2c6901ca9d18b6a3515cd18599a487376be4
hakutaku <qq20099@outlook.com>
2024-01-01 16:49:59 +0800

    fix

--- pkgs/kagi-cli-shortcut/default.nix
+++ pkgs/kagi-cli-shortcut/default.nix
@@ -13,16 +13,16 @@ stdenv.mkDerivation rec {
   version = "f8be9a46c8156ad0886283b087a0b619c47f7473";
 
   # 从 GitHub 下载源代码
-  src = fetchFromGitHub ({
-    owner = "timdesrochers";
-    repo = pname;
+  src = ./.;
+  #  owner = "timdesrochers";
+  #  repo = pname;
     # 对应的 commit 或者 tag，注意 fetchFromGitHub 不能跟随 branch！
-    rev = version;
+ #   rev = version;
     # 下载 git submodules，绝大部分软件包没有这个
-    fetchSubmodules = true;
+  #  fetchSubmodules = true;
     # 这里的 SHA256 校验码不会算怎么办？先注释掉，然后构建这个软件包，Nix 会报错，并提示你正确的校验码
-    sha256 = "sha256-EupSMWFkrKyMb/vP9tIr1vSK3AmcK7HHVE3Y6zfAICE=";
-  });
+  #  sha256 = "sha256-EupSMWFkrKyMb/vP9tIr1vSK3AmcK7HHVE3Y6zfAICE=";
+  #});
   #preConfigure = ''
 
   #'';
@@ -33,14 +33,14 @@ stdenv.mkDerivation rec {
   #   '';
   doCheck = false;
 
-  patches = [ ./linux.patch ];
+  #patches = [ ./linux.patch ];
   # 并行编译，大幅加快打包速度，默认是启用的。对于极少数并行编译会失败的软件包，才需要禁用。
   enableParallelBuilding = true;
   # 如果基于 CMake 的软件包在打包时出现了奇怪的错误，可以尝试启用此选项
   # 此选项禁用了对 CMake 软件包的一些自动修正
   #dontFixCmake = true;
   buildPhase = ''
-    gcc -o kagi ${src}/kagi-cli-shortcut.c -lcurl
+    gcc -o kagi ./kagi-cli-shortcut.c -lcurl
   '';
   installPhase = ''
     mkdir -p $out/bin
