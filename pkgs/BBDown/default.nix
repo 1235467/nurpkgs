209157cff5d0420c40e64d1a239cb5e8f1523518
hakutaku <qq20099@outlook.com>
2024-01-18 17:43:54 +0800

    try fix BBDown

--- pkgs/BBDown/default.nix
+++ pkgs/BBDown/default.nix
@@ -3,6 +3,8 @@
 #, buildDotnetGlobalTool
 , dotnetCorePackages
 , callPackage
+, makeWrapper
+, ffmpeg
 , ...
 }:
 let
@@ -16,10 +18,19 @@ in
   inherit pname version nugetSha256;
   dotnet-sdk = dotnetCorePackages.sdk_8_0;
   dotnet-runtime = dotnetCorePackages.sdk_8_0;
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+  buildInputs = [
+    ffmpeg
+  ];
   postInstall = ''
     mkdir -p $out/lib/BBDown/
     ln -s /home/hakutaku/.config/BBDown/BBDown.data $out/lib/BBDown/
+    wrapProgram $out/bin/BBDown \
+        --prefix PATH : ${lib.makeBinPath [ ffmpeg ]}
   '';
+
   meta = with lib; {
     description = "Bilibili Downloader";
     homepage = "https://github.com/nilaoda/BBDown";
