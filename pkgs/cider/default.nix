d3576799910253666a88faed0574e7bf8384e029
hakutaku <qq20099@outlook.com>
2024-01-24 20:33:29 +0800

    update

--- pkgs/cider/default.nix
+++ pkgs/cider/default.nix
@@ -1,18 +1,18 @@
 { appimageTools, lib, fetchurl }:
-let
-  patch-4_3_0-rc = fetchurl {
-    url = "https://cider.m5y6.c17.e2-5.dev/4.3.0-rc.zip";
-    sha256 = "";
-  };
-in
+#let
+#  patch-4_3_0-rc = fetchurl {
+#    url = "https://cider.m5y6.c17.e2-5.dev/4.3.0-rc.zip";
+#    sha256 = "";
+#  };
+#in
 appimageTools.wrapType2 rec {
   pname = "cider";
-  version = "2.3.9";
+  version = "2.3.0-RC15";
   name = "${pname}";
 
   src = fetchurl {
     url = "https://cider.m5y6.c17.e2-5.dev/Cider-${version}.AppImage";
-    sha256 = "sha256-1O1YIAZVqrlAnD3RDZP4ULt44S0+w9NciN1Z+9oFiRQ=";
+    sha256 = "";
   };
 
   extraInstallCommands =
