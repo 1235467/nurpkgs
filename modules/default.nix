c402969b39069a765e33533db290238547eb0df1
hakutaku <qq20099@outlook.com>
2024-03-22 17:55:09 +0800

    edit some stuff

--- modules/default.nix
+++ modules/default.nix
@@ -2,5 +2,6 @@
   # Add your NixOS modules here
   #
   # my-module = ./my-module;
-  sunshine = ./sunshine.nix;
+  sunshine = ./nixos/sunshine.nix;
+  nvidia = ./nixos/nvidia.nix
 }
---
+++ modules/nixos/nvidia.nix
@@ -0,0 +1,24 @@
+{ config, lib, pkgs, inputs, ... }:
+
+let
+  cfg = config.nvk;
+in
+{
+  options.nvk = {
+  enable = lib.mkOption {
+        default = false;
+        type = lib.types.bool;
+        description = lib.mdDoc ''
+          Select which NVIDIA driver to use
+
+            Possible values:
+            `false`: Official driver, a bit buggy.
+            `true`: Open source driver, under heavy development, unstable.
+        '';
+      };
+  };
+  config.boot.kernelParams = if cfg.enable then ["nouveau.config=NvGspRm=1" "acpi_backlight=vendor" "nouveau.debug=info,VBIOS=info,gsp=debug"] else [];
+  config.services.xserver.videoDrivers = if cfg.enable then ["nouveau"] else ["nvidia"];
+  config.chaotic.mesa-git.enable = if cfg.enable then lib.mkForce true else lib.mkForce false;
+}
+
