4135f589f3a59a82febba776bea5e03b9f95ae9d
hakutaku <qq20099@outlook.com>
2024-03-31 23:07:16 +0800

    upgrade waylyrics

--- pkgs/waylyrics/default.nix
+++ pkgs/waylyrics/default.nix
@@ -3,18 +3,18 @@
 rustPlatform.buildRustPackage rec
 {
   pname = "waylyrics";
-  version = "051b8bf94a7264b9557086eb0d357b4b758f865f";
+  version = "0.2.13";
 
   src = fetchFromGitHub {
     owner = "poly000";
     repo = pname;
-    rev = version;
-    hash = "sha256-qBFhbRcYwpBIH50reNb62hQ2M6wxI0t8Uk7DEKtFl3A=";
+    rev = "v${version}";
+    hash = "sha256-522NdpGj0oh2SbWa4GFCFpqNFRhqQxfZ1ZRuS9jUj7Y=";
   };
 
   cargoLock.lockFile = ./Cargo.lock;
   cargoLock.outputHashes = {
-    "ncmapi-0.1.13" = "sha256-wh9RsyuS1L7rnz1jh2A27s6wUvyH8cNgUywPORIimmg=";
+    "ncmapi-0.1.13" = "sha256-qu89qf4IPM14V+oE4QQr/SsXSTx3vQbyfzD+Pihcd3E=";
     "qqmusic-rs-0.1.0" = "sha256-woLsO0n+m3EBUI+PRLio7iLp0UPQSliWK0djCSZEaZc=";
   };
 
