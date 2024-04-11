188a8c2daf1123240852441625f7ec6e4a5a9012
hakutaku <qq20099@outlook.com>
2024-04-11 23:03:52 +0800

    disable gpu check for sway

--- pkgs/Overrides/sway-im/default.nix
+++ pkgs/Overrides/sway-im/default.nix
@@ -11,6 +11,7 @@ pkgs.sway.override (previous: {
     sway-unwrapped = previous.sway-unwrapped.overrideAttrs (previousAttrs: rec {
       patches = previousAttrs.patches ++
     [
+    ./unsupported-gpu.patch
     "${sources.sway-im.src}/0001-text_input-Implement-input-method-popups.patch"
     "${sources.sway-im.src}/0002-chore-fractal-scale-handle.patch"
     "${sources.sway-im.src}/0003-chore-left_pt-on-method-popup.patch"
