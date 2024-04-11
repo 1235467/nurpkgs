c0b7202c1433ae78f08e6322c803c995cfbab07b
hakutaku <qq20099@outlook.com>
2024-04-11 22:08:59 +0800

    ....

--- pkgs/Overrides/sway-im/default.nix
+++ pkgs/Overrides/sway-im/default.nix
@@ -7,13 +7,13 @@
 let
   sources = pkgs.callPackage ../../../_sources/generated.nix { };
 in
-pkgs.sway-unwrapped.overrideAttrs (
-  attrs: rec {
-    patches = attrs.patches ++
+pkgs.sway.override (previous: {
+    sway-unwrapped = previous.sway-unwrapped.overrideAttrs (previousAttrs: rec {
+      patches = previousAttrs.patches ++
     [
     "${sources.sway-im.src}/0001-text_input-Implement-input-method-popups.patch"
     "${sources.sway-im.src}/0002-chore-fractal-scale-handle.patch"
     "${sources.sway-im.src}/0003-chore-left_pt-on-method-popup.patch"
   ];
-  }
-)
+  });
+})
