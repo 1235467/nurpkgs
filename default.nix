0740eb78afbd0db621c2769c2043469d7d565c36
hakutaku <qq20099@outlook.com>
2024-05-06 20:09:01 +0800

    add pkg: sway-im-git

--- default.nix
+++ default.nix
@@ -81,6 +81,7 @@ rec {
   qcm = pkgs.qt6.callPackage ./pkgs/Overrides/qcm {};
   mpv = pkgs.wrapMpv (pkgs.mpv.unwrapped.override { cddaSupport = true; }) {scripts = [ pkgs.mpvScripts.mpris ];};
   sway-im = pkgs.callPackage ./pkgs/Overrides/sway-im {};
+  sway-im-git = pkgs.callPackage ./pkgs/Overrides/sway-im-git {};
   #hyprland = pkgs.callPackage ./pkgs/Overrides/hyprland {};
   pot = pkgs.callPackage ./pkgs/Overrides/pot {};
 
