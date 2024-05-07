f863fe9604f3005509ef72801a68d4cb84545af5
hakutaku <qq20099@outlook.com>
2024-05-07 09:09:09 +0800

    add pkg: llamafile(broken)

--- pkgs/Overrides/sway-git/default.nix
+++ pkgs/Overrides/sway-git/default.nix
@@ -12,7 +12,9 @@ pkgs.sway.override (previous: {
       inherit (sources.sway) version src;
       patches = previousAttrs.patches ++
     [
-    "${sources.sway-im-git.src}/7226.patch"
+    #"${sources.sway-im.src}/0001-text_input-Implement-input-method-popups.patch"
+    #"${sources.sway-im.src}/0002-chore-fractal-scale-handle.patch"
+    #"${sources.sway-im.src}/0003-chore-left_pt-on-method-popup.patch"
   ];
   });
 })
