5a4433fea03920eb8d7888dca9fe9fab564ee21d
hakutaku <qq20099@outlook.com>
2024-10-17 22:38:01 +0800

    ...

--- pkgs/Overrides/linux_6_10_sched-ext/default.nix
+++ pkgs/Overrides/linux_6_10_sched-ext/default.nix
@@ -5,10 +5,11 @@
 }:
 pkgs.linux_6_10.override {
 extraConfig = ''
-          CONFIG_SCHED_CLASS_EXT=y
-          CONFIG_BPF_SYSCALL=y
-          CONFIG_BPF_JIT=y 
-	  CONFIG_DEBUG_INFO_BTF=y
-          CONFIG_BPF_JIT_ALWAYS_ON=y CONFIG_PAHOLE_HAS_BTF_TAG=y 
+          CONFIG_SCHED_CLASS_EXT y
+          CONFIG_BPF_SYSCALL y
+          CONFIG_BPF_JIT y 
+	  CONFIG_DEBUG_INFO_BTF y
+          CONFIG_BPF_JIT_ALWAYS_ON y
+          CONFIG_PAHOLE_HAS_BTF_TAG y 
         '';
 }
