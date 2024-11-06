4faa657489a44b26e809e6a8f6f5115baca7851c
hakutaku <qq20099@outlook.com>
2024-11-06 15:44:56 +0800

    ?????

--- flake.nix
+++ flake.nix
@@ -19,21 +19,22 @@
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
     in
     rec {
-      
-      aichat = dream2nix.lib.evalModules {
+      dream2nix-pkgs = {
+        aichat = dream2nix.lib.evalModules {
         packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
         modules = [
           # Import our actual package definiton as a dream2nix module from ./default.nix
-          ./pkgs-by-lang/Rust/aichat/default.nix
-          {
-            # Aid dream2nix to find the project root. This setup should also works for mono
-            # repos. If you only have a single project, the defaults should be good enough.
-            paths.projectRoot = ./.;
-            # can be changed to ".git" or "flake.nix" to get rid of .project-root
-            paths.projectRootFile = "flake.nix";
-            paths.package = ./.;
-          }
-        ];
+            ./pkgs-by-lang/Rust/aichat/default.nix
+            {
+              # Aid dream2nix to find the project root. This setup should also works for mono
+              # repos. If you only have a single project, the defaults should be good enough.
+              paths.projectRoot = ./.;
+              # can be changed to ".git" or "flake.nix" to get rid of .project-root
+              paths.projectRootFile = "flake.nix";
+              paths.package = ./.;
+            }
+          ];
+        };
       };
       legacyPackages = forAllSystems (
         system:
