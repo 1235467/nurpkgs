9d4aa58c2c79d4fa319c6a7fbba1a94ff08c2be1
hakutaku <qq20099@outlook.com>
2025-01-13 22:47:18 +0800

    update cider

--- pkgs/AppImage/cider/default.nix
+++ pkgs/AppImage/cider/default.nix
@@ -1,10 +1,10 @@
 { appimageTools, lib, pkgs, }:
 appimageTools.wrapType2 rec {
   pname = "cider";
-  version = "2.4.0";
+  version = "2.6.0";
 
   src = pkgs.fetchurl {
-    url = "https://fly.storage.tigris.dev/cider/Cider-${version}.AppImage";
+    url = "https://fly.storage.tigris.dev/cider/Cider-${version}-x64.AppImage";
     sha256 = "sha256-PSOJYaxEGahqX/7XIKkAiObqHwuAu6YI7CF/qc9DOZU=";
   };
 
@@ -15,7 +15,7 @@ appimageTools.wrapType2 rec {
     ''
       install -m 444 -D ${contents}/${pname}.desktop -t $out/share/applications
       substituteInPlace $out/share/applications/${pname}.desktop \
-        --replace 'Exec=AppRun' 'Exec=${pname}'
+        --replace-quiet 'Exec=AppRun' 'Exec=${pname}'
       cp -r ${contents}/usr/share/icons $out/share
     '';
 
