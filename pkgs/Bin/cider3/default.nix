e1eebc9246195e3b6fa7ba6e1f602ae69ff02231
hakutaku <qq20099@outlook.com>
2025-12-20 13:46:26 +0800

    update cider 3 (warning: after this update, valid license of cider 3 is required sadly)

--- pkgs/Bin/cider3/default.nix
+++ pkgs/Bin/cider3/default.nix
@@ -9,8 +9,8 @@
 
 let
   src = fetchurl {
-    url = "https://repo.cider.sh/arch/cider-v3.0.0-linux-x64.pkg.tar.zst";
-    sha256 = "sha256-+Rdak70dPmL/+DV5LhEY94ypEUz9FhxhOwVQ2V2Q/Xg=";
+    url = "https://repo.cider.sh/arch/cider-v3.1.8-linux-x64.pkg.tar.zst";
+    sha256 = "sha256-u8Ax2NqKxHhmzmm4eGUi6XpfPXHy6FndujtplnUfze4=";
   };
   resource = stdenv.mkDerivation rec {
     name = "cider-pkg";
@@ -38,7 +38,7 @@ in
   
 stdenv.mkDerivation {
   pname = "cider";
-  version = "3.0.0";
+  version = "3.1.8";
   phases = [ "installPhase" ];
   nativeBuildInputs = [ autoPatchelfHook makeWrapper ];
   installPhase = ''
