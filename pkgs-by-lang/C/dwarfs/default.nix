4f57d2fdae9ab9f07c975158c99f0566f9ed1ee4
hakutaku <qq20099@outlook.com>
2026-01-24 20:14:55 +0800

    ?

--- pkgs-by-lang/C/dwarfs/default.nix
+++ pkgs-by-lang/C/dwarfs/default.nix
@@ -98,24 +98,6 @@ stdenv.mkDerivation (finalAttrs: {
     # refer to symbols that only exist in `.so` files compiled with that version.
     (gtest.override { cxx_standard = "20"; })
   ];
-  # these fail inside of the sandbox due to missing access
-  # to the FUSE device
-  GTEST_FILTER =
-    let
-      disabledTests = [
-        "dwarfs/tools_test.end_to_end/*"
-        "dwarfs/tools_test.mutating_and_error_ops/*"
-        "dwarfs/tools_test.categorize/*"
-        "dwarfs/tools_test.timestamps_fuse*"
-        "dwarfs/tools_test.dwarfs_automount*"
-        "dwarfs/tools_test.dwarfs_fsname_and_subtype*"
-        "dwarfs/tools_test.dwarfs_image_size*"
-        "dwarfs/tools_test.dwarfs_obsolete_options*"
-        "dwarfs/sparse_files_test.*"
-      ];
-    in
-    "-${lib.concatStringsSep ":" disabledTests}";
-
   doInstallCheck = false;
   nativeInstallCheckInputs = [ versionCheckHook ];
   versionCheckProgram = "${placeholder "out"}/bin/dwarfs";
